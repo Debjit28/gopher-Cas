0cf4e10a042a09ec4b247269b696c5c70ebe3716
Debjit28 <debjit2807@gmail.com>
2026-07-30 12:13:13 +0530

    feat: started project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Debjit28/gopher-Cas
+
+go 1.26.1
---
+++ p2p/transport.go
@@ -0,0 +1,12 @@
+package p2p
+
+// peer is just that representing the remote node
+
+type Peer interface {
+}
+
+// Transport is anything that handles communication
+// between  the nodes in the network .
+// it follow protocols like tcp udp 	websockets.
+type Transport interface {
+}
