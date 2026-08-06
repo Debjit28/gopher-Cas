5898dbaa4089ba2cee6bee3207174a636f54be51
Debjit28 <debjit2807@gmail.com>
2026-08-06 19:52:32 +0530

    feat(transportation.go) accept TCP connections and log raw byte payloads

--- p2p/message.go
+++ p2p/message.go
@@ -1,1 +1,6 @@
 package p2p
+
+// Message represent that is being sent over each transport between 2 nodes in za network
+type Message struct {
+	Payload []byte
+}
--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -95,15 +95,27 @@ func (t *TCPTransport) handleConnection(conn net.Conn) {
 	}
 
 	//Read Loop
-	msg := &Temp{}
+
+	buf := make([]byte, 2000)
+	// msg := &Temp{}
 	for {
 
-		if err := t.Decoder.Decode(conn, msg); err != nil {
+		n, err := conn.Read(buf)
+
+		if err != nil {
 
 			fmt.Printf("TCP Error : %s\n", err)
-			continue
 
 		}
+
+		// if err := t.Decoder.Decode(conn, msg); err != nil {
+
+		// 	fmt.Printf("TCP Error : %s\n", err)
+		// 	continue
+
+		// }
+
+		fmt.Printf("message %+v\n", buf[:n])
 
 	}
 
