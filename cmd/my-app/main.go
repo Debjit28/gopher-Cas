8ee00e047b182831990cbbf42e01e1151cfca240
Debjit28 <debjit2807@gmail.com>
2026-08-06 19:21:27 +0530

    commit

--- cmd/my-app/main.go
+++ cmd/my-app/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/Debjit28/gopher-Cas/p2p"
 )
 
 func main() {
 	opts := p2p.TCPTransportOpt{
-		ListenAddr: ":3000",
+		ListenAddr:    ":3000",
+		HandshakeFunc: p2p.NOPHandshakeFunc,
+		Decoder:       p2p.GOBDecoder{},
 	}
 
 	tr := p2p.NewTCPTransport(opts)
 
 	if err := tr.ListenAndAccept(); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// Keep main process alive while accepting connections
